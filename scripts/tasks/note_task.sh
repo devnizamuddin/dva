d706c9b5a0b3c763d9f4fe18a100ec70e2756f3b
devnizamuddin <dev.nizamuddin@gmail.com>
2025-08-25 15:59:09 +0600

    Release v1.0.4 - First stable version

--- scripts/tasks/note_task.sh
+++ scripts/tasks/note_task.sh
@@ -2,13 +2,14 @@
 
 
 function print_note_task_list() {
-  local ORANGE="\033[38;5;208m"
-  local BG_ORANGE="\033[48;5;208m"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}┌───────────────────────────────────────────────┐${NC}"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}│                                               │${NC}"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}│ 1. Remote Url                                 │${NC}"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}│                                               │${NC}"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}│ 2. Active Branches                            │${NC}"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}│                                               │${NC}"
-  echo -e "${BG_ORANGE}${ORANGE}${BOLD}└───────────────────────────────────────────────┘${NC}"
+  local WHITE="\033[1;37m"
+  local BG_WHITE="\033[47m"
+
+  echo -e "${BG_WHITE}${WHITE}${BOLD}┌───────────────────────────────────────────────┐${NC}"
+  echo -e "${BG_WHITE}${WHITE}${BOLD}│                                               │${NC}"
+  echo -e "${BG_WHITE}${WHITE}${BOLD}│ 1. Remote Url                                 │${NC}"
+  echo -e "${BG_WHITE}${WHITE}${BOLD}│                                               │${NC}"
+  echo -e "${BG_WHITE}${WHITE}${BOLD}│ 2. Active Branches                            │${NC}"
+  echo -e "${BG_WHITE}${WHITE}${BOLD}│                                               │${NC}"
+  echo -e "${BG_WHITE}${WHITE}${BOLD}└───────────────────────────────────────────────┘${NC}"
 }
